0c089856cbace9c7ca66f680c8d38af026d451ee
Surinder Singh Matoo <surinder83singh@gmail.com>
2024-03-09 06:49:02 +0530

    Sender.tab info

--- src/bindings.rs
+++ src/bindings.rs
@@ -11,7 +11,20 @@ extern "C" {
     #[wasm_bindgen(method, getter)]
     pub fn id(this: &Sender) -> Option<String>;
 
+    #[wasm_bindgen(method, getter)]
+    pub fn tab(this: &Sender) -> TabInfo;
+
     #[wasm_bindgen(catch, js_namespace = ["chrome", "runtime"], js_name="sendMessage")]
     pub async fn send_message(s: &JsValue) -> Result<JsValue, JsValue>;
 
+
+    #[wasm_bindgen(extends = Object)]
+    #[derive(Debug)]
+    pub type TabInfo;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn id(this: &TabInfo) -> u32;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn url(this: &TabInfo) -> String;
 }
